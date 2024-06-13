baffa051a60b49e3ba1b069b079c9efeaf53b130
Mario Miličević <mmilic05@fesb.hr>
2024-06-13 14:39:46 +0200

    Vercel cache fix

--- app/api/fetchtrans/route.ts
+++ app/api/fetchtrans/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const transactions = await prisma.transaction.findMany();
     return NextResponse.json({
