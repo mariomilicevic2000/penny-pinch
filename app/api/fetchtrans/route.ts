da3686a6a0f25d4a327756c2dcd4df6443d4d662
Mario Miličević <mmilic05@fesb.hr>
2024-06-13 13:36:19 +0200

    Rewrote fetch api

--- app/api/fetchtrans/route.ts
+++ app/api/fetchtrans/route.ts
@@ -1,26 +1,21 @@
-// pages/api/fetchTransactions.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
-import prisma from '@/lib/db';
-import { NextApiRequest, NextApiResponse } from 'next';
+const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const transactions = await prisma.transaction.findMany();
-      res.status(200).json({
-        message: 'Successfully fetched transactions',
-        status: 200,
-        data: transactions
-      });
-    } catch (error) {
-      console.error('Error fetching transactions:', error);
-      res.status(500).json({
-        message: 'Failed to fetch transactions',
-        status: 500
-      });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function GET(req: NextRequest) {
+  try {
+    const transactions = await prisma.transaction.findMany();
+    return NextResponse.json({
+      message: 'Successfully fetched transactions',
+      status: 200,
+      data: transactions,
+    });
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    return NextResponse.json({
+      message: 'Failed to fetch transactions',
+      status: 500,
+    });
   }
 }
